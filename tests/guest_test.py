481a7ac19cfbbb83ae639220cb4f018904991b6a
CordiiT14 <cordelia.toennies@live.com>
2022-03-04 15:00:23 +0000

    4 successfull guest attribute tests

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -11,3 +11,15 @@ class TestGuest(unittest.TestCase):
         self.guest5 = Guest("Bex")
         self.guest6 = Guest("Katie")
         self.guest7 = Guest("Kev")
+
+    def test_guest_has_name(self):
+        self.assertEqual("Ella", self.guest1.name)
+    
+    def test_guest_has_name_2(self):
+        self.assertEqual("Adam", self.guest2.name)
+
+    def test_guest_has_name_3(self):
+        self.assertEqual("Laila", self.guest3.name)
+    
+    def test_guest_has_name_4(self):
+        self.assertEqual("Robbie", self.guest4.name)
